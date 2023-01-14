a063fdc91186eb9159903b35b41e33fb8936876a
Bilal Babale <bilalbabale@gmail.com>
2023-01-14 12:59:50 +0100

    Modify Gameboard class to comply with C++ best practices
    C++ best prectice is class methods definition should take place outside
    the class, only method declaration should be inside the class.

--- gameboard.hpp
+++ gameboard.hpp
@@ -5,34 +5,42 @@ class Gameboard{
         char board[4][4];
 
     public:
-        Gameboard(){
-            for(int i=0; i < 4; i++){
-                for(int j=0; j < 4; j++){
-                    board[i][j] = '-';
-                }
-            }
-        }
-
-        void print(){
-            for(int i=0; i < 4; i++){
-                for(int j=0; j < 4; j++){
-                    cout << board[i][j];
-                }
-                cout << endl;
-            }
-        }
-
-        void setSquare(int row, int col, char val){
-            if(board[row][col] == '-'){
-                board[row][col] = val;
-            }else{
-                cout << "Selected Square is already occupied";
-            }
-        }
-
-        char getSquare(int row, int col){
-            return board[row][col];
-        }
-
+        Gameboard();
+        void print();
+        void setSquare(int row, int col, char val);
+        char getSquare(int row, int col);
 
 };
+
+
+Gameboard::Gameboard(){
+    for(int i=0; i < 4; i++){
+        for(int j=0; j < 4; j++){
+            board[i][j] = '-';
+        }
+    }
+}
+
+
+void Gameboard::print(){
+    for(int i=0; i < 4; i++){
+        for(int j=0; j < 4; j++){
+            cout << board[i][j];
+        }
+        cout << endl;
+    }
+}
+
+
+void Gameboard::setSquare(int row, int col, char val){
+    if(board[row][col] == '-'){
+        board[row][col] = val;
+    }else{
+        cout << "Selected Square is already occupied";
+    }
+}
+
+
+char Gameboard::getSquare(int row, int col){
+    return board[row][col];
+}
